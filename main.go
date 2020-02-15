7506476f14fcecd50c7d239fb661d550537d15a6
imam98 <imannududin@outlook.com>
2020-02-16 00:09:33 +0700

    Implement command multiplexer

--- main.go
+++ main.go
@@ -6,19 +6,17 @@ import (
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"imam.miniproject/akimoto-ai/handlers"
+	"imam.miniproject/akimoto-ai/models"
 )
 
 var isAskingWeather = false
 var quotes []string
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_BOT_TOKEN"))
+	mux, err := models.NewCommandRouter(os.Getenv("TG_BOT_TOKEN"), true)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-
-	bot.Debug = true
-	log.Printf("Authorized on account %v\n", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -28,47 +26,50 @@ func main() {
 		log.Fatalln("Error parsing file:", err.Error())
 	}
 
-	updates, err := bot.GetUpdatesChan(u)
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
+	mux.HandleCommand("help", handlers.HelpHandler)
+	mux.Serve(u)
 
-		if update.Message.IsCommand() {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-			switch update.Message.Command() {
-			case "weather":
-				if !isAskingWeather {
-					msg.Text = "Alright I'm gonna process the data after you tell me your location"
-					isAskingWeather = true
-				} else {
-					msg.Text = "You've already asked that a moment ago, please be kind and send me your location so I can start my work 😇"
-				}
-				bot.Send(msg)
-			case "help":
-				handlers.HelpHandler(update, bot)
-			case "cheermeup":
-				handlers.CheermeupHandler(update, bot, quotes)
-			default:
-				msg.Text = "Sorry, I don't acknowledge that command. Please try another one."
-				bot.Send(msg)
-			}
-		} else {
-			if isAskingWeather {
-				if loc := update.Message.Location; loc != nil {
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-					msg.Text = "Processing data, may take a while..."
-					bot.Send(msg)
+	// updates, err := bot.GetUpdatesChan(u)
+	// for update := range updates {
+	// 	if update.Message == nil {
+	// 		continue
+	// 	}
 
-					msg.Text = handlers.GenWeatherMsg(loc)
-					msg.ParseMode = "markdown"
-					bot.Send(msg)
+	// 	if update.Message.IsCommand() {
+	// 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+	// 		switch update.Message.Command() {
+	// 		case "weather":
+	// 			if !isAskingWeather {
+	// 				msg.Text = "Alright I'm gonna process the data after you tell me your location"
+	// 				isAskingWeather = true
+	// 			} else {
+	// 				msg.Text = "You've already asked that a moment ago, please be kind and send me your location so I can start my work 😇"
+	// 			}
+	// 			bot.Send(msg)
+	// 		case "help":
+	// 			handlers.HelpHandler(update, bot)
+	// 		case "cheermeup":
+	// 			handlers.CheermeupHandler(update, bot, quotes)
+	// 		default:
+	// 			msg.Text = "Sorry, I don't acknowledge that command. Please try another one."
+	// 			bot.Send(msg)
+	// 		}
+	// 	} else {
+	// 		if isAskingWeather {
+	// 			if loc := update.Message.Location; loc != nil {
+	// 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+	// 				msg.Text = "Processing data, may take a while..."
+	// 				bot.Send(msg)
 
-					isAskingWeather = false
-				} else {
-					isAskingWeather = false
-				}
-			}
-		}
-	}
+	// 				msg.Text = handlers.GenWeatherMsg(loc)
+	// 				msg.ParseMode = "markdown"
+	// 				bot.Send(msg)
+
+	// 				isAskingWeather = false
+	// 			} else {
+	// 				isAskingWeather = false
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
---
+++ models/multiplexer.go
@@ -0,0 +1,59 @@
+package models
+
+import (
+	"log"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+type Multiplexer struct {
+	bot    *tgbotapi.BotAPI
+	routes map[string]Handler
+}
+
+type Handler func(update tgbotapi.Update, bot *tgbotapi.BotAPI)
+
+func NewCommandRouter(token string, debugMode bool) (Multiplexer, error) {
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		return Multiplexer{}, err
+	}
+
+	bot.Debug = debugMode
+	mux := Multiplexer{
+		bot:    bot,
+		routes: map[string]Handler{},
+	}
+
+	log.Printf("Authorized on account %v\n", bot.Self.UserName)
+	return mux, nil
+}
+
+func (mux *Multiplexer) HandleCommand(command string, handler Handler) {
+	mux.routes[command] = handler
+}
+
+func (mux *Multiplexer) Serve(updateConfig tgbotapi.UpdateConfig) error {
+	updates, err := mux.bot.GetUpdatesChan(updateConfig)
+	if err != nil {
+		return err
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.IsCommand() {
+			if handler, ok := mux.routes[update.Message.Command()]; ok {
+				handler(update, mux.bot)
+			} else {
+				id := update.Message.Chat.ID
+				msg := tgbotapi.NewMessage(id, "Sorry, I don't acknowledge that command. Please try another one.")
+				mux.bot.Send(msg)
+			}
+		}
+	}
+
+	return nil
+}
